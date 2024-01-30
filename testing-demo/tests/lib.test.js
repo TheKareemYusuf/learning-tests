0566e638dabc370c1e9bd687c3a107fed2e1c836
Yusuf <yakande96@gmail.com>
2024-01-30 08:56:01 +0100

    added more unit tests

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -25,10 +25,35 @@ describe('Greet', () => {
      })
 })
 
-describe('Array', () => { 
-    test('should test array', () => {
+describe('getCurrencies', () => { 
+    it('should test array', () => {
         const result = lib.getCurrencies();
 
         expect(result).toEqual(expect.arrayContaining(['EUR', 'USD', 'AUD']))
     })
  })
+
+describe('getProduct', () => {
+    it('should return the product with the given id', () => { 
+        const result = lib.getProduct(1);
+
+        expect(result).toMatchObject({id: 1, price: 10})
+
+        expect(result).toHaveProperty('id', 1)
+     })
+ })
+
+ describe('registerUser', () => { 
+    it('should throw error if username is falsy', () => { 
+         const args = [null, undefined, '', 0, NaN, false ]
+         args.forEach(a => {
+            expect(() => {lib.registerUser(a)}).toThrow()
+         })
+     })
+
+     it('should return a user object if valid username is passed', () => {
+        const result = lib.registerUser('mosh')
+        expect(result).toMatchObject({username: 'mosh'})
+        expect(result.id).toBeGreaterThan(0)
+     })
+  })
